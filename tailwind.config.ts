50c6524b5f2108511fca2e37f3d2290db2ae25a4
Gustavo Henrique <gustavohgustavo@hotmail.com>
2024-02-13 16:50:36 -0300

    feature: teste

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,13 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    screens: {
+      "xsm": { "max": "320px" },
+      "sm": { "max": "576px" },
+      "md": { "max": "768px" },
+      "l": { "max": "992px" },
+      "xl": { "max": "1280px" },
+    }
   },
   plugins: [],
 };
